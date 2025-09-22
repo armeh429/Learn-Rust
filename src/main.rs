163b70cf5cb03273304c98cf41a459b05ae2ec5e
armeh429 <armeh.rostami@gmail.com>
2025-09-22 15:57:36 +0330

    Added input example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
-use std::ops::Range;
+
 
 fn main() {
-    let rang_1: Range<i32> = 1..5;
-    for num in rang_1 {
-        println!("{}", num);
+    
+    let mut number: Vec<i32> = Vec::new();
+    println!("hit 0 to exit, or enter a number to add to the list");
+    
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("failed to read line");
+        let input: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        if input == 0 {
+            break;
+        }
+        
+        number.push(input);
     }
+
+    let mut even_count: i32 = 0;
+    let mut index_number: usize = 0;
+    while index_number < number.len() {
+        if number[index_number] % 2 == 0 {
+            even_count += 1;
+        }
+        index_number += 1;
+    }
+    let mut summ:i32 = 0;
+    for num in &number {
+        summ += num;
+    }
+    println!("The sum of the numbers is: {}", summ);
+    println!("You entered {} even numbers", even_count);
+    println!("You entered: {:?}", number);
+
 }
